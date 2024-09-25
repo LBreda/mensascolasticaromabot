af728d990700d282f31edc2dedf370f4b6462c94
Lorenzo Breda <lorenzo@lbreda.com>
2024-09-25 22:52:39 +0200

    :recycle: Messe a fattor comune funzioni quasi identiche

--- app/Telegram/Commands/Domani.php
+++ app/Telegram/Commands/Domani.php
@@ -5,43 +5,19 @@ namespace App\Telegram\Commands;
 use App\MensaComune\Client;
 use App\Models\Municipio;
 use App\Models\NotificationRequest;
+use Carbon\Carbon;
 use http\Client\Curl\User;
 use Illuminate\Database\Eloquent\Collection;
 use Telegram\Bot\Commands\Command;
 use Telegram\Bot\Keyboard\Keyboard;
 
-class Domani extends Command
+class Domani extends Oggi
 {
     protected string $name = 'domani';
     protected string $description = 'Ottiene il menu di domani';
-
-    /**
-     * Shows the menu to for today
-     * @return void
-     * @throws \Illuminate\Http\Client\ConnectionException
-     */
-    public function handle(): void
+    protected function menuDate(): Carbon
     {
-        // Sends the notifications
-        $user = \App\Models\User::firstWhere(['telegram_id' => $this->getUpdate()->message->from->id]);
-
-        $atLeastOneMessage = false;
-        $user->notification_requests->each(function (NotificationRequest $nr) use ($user, &$atLeastOneMessage) {
-            $menu = Client::getMenu($nr->municipio_id, $nr->grado_id, now()->addDay());
-            if ($menu) {
-                \Telegram::sendMessage([
-                    'chat_id' => $this->getUpdate()->message->chat->id,
-                    'text'    => $menu,
-                    'parse_mode' => 'HTML'
-                ]);
-                $atLeastOneMessage = true;
-            }
-        });
-        if(!$atLeastOneMessage) {
-            \Telegram::sendMessage([
-                'chat_id' => $this->getUpdate()->message->chat->id,
-                'text'    => 'Non è presente alcun menu per domani.',
-            ]);
-        }
+        return now()->addDay();
     }
+    protected string $emptyMessage = 'Non è presente alcun menu per domani.';
 }
--- app/Telegram/Commands/Oggi.php
+++ app/Telegram/Commands/Oggi.php
@@ -5,6 +5,7 @@ namespace App\Telegram\Commands;
 use App\MensaComune\Client;
 use App\Models\Municipio;
 use App\Models\NotificationRequest;
+use Carbon\Carbon;
 use http\Client\Curl\User;
 use Illuminate\Database\Eloquent\Collection;
 use Telegram\Bot\Commands\Command;
@@ -14,6 +15,11 @@ class Oggi extends Command
 {
     protected string $name = 'oggi';
     protected string $description = 'Ottiene il menu di oggi';
+    protected function menuDate(): Carbon
+    {
+        return now();
+    }
+    protected string $emptyMessage = 'Non è presente alcun menu per oggi.';
 
     /**
      * Shows the menu to for today
@@ -26,12 +32,13 @@ class Oggi extends Command
         $user = \App\Models\User::firstWhere(['telegram_id' => $this->getUpdate()->message->from->id]);
 
         $atLeastOneMessage = false;
-        $user->notification_requests->each(function (NotificationRequest $nr) use ($user, &$atLeastOneMessage) {
-            $menu = Client::getMenu($nr->municipio_id, $nr->grado_id);
+        $user->notification_requests->each(function (NotificationRequest $nr) use (&$user, &$atLeastOneMessage) {
+            $user = $user->refreshTelegramInfo();
+            $menu = Client::getMenu($nr->municipio_id, $nr->grado_id, $this->menuDate());
             if ($menu) {
                 \Telegram::sendMessage([
                     'chat_id' => $this->getUpdate()->message->chat->id,
-                    'text'    => $menu,
+                    'text'    => (($user->telegram_user_data['first_name'] ?? false) ? "Ciao {$user->telegram_user_data['first_name']}!\n\n" : "Ciao!\n\n") . $menu,
                     'parse_mode' => 'HTML'
                 ]);
                 $atLeastOneMessage = true;
@@ -40,7 +47,7 @@ class Oggi extends Command
         if(!$atLeastOneMessage) {
             \Telegram::sendMessage([
                 'chat_id' => $this->getUpdate()->message->chat->id,
-                'text'    => 'Non è presente alcun menu per oggi.',
+                'text'    => $this->emptyMessage,
             ]);
         }
     }
